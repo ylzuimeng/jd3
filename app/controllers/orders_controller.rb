7b75c590a25a018e33fbc130f793fcfff9553b6e
ylzuimeng <ylzuimeng@gmail.com>
2017-11-07 09:30:02 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,11 @@ class OrdersController < ApplicationController
         product_list.order = @order
         product_list.product_name = cart_item.product.title
         product_list.product_price = cart_item.product.price
-        product_quantity = cart_item.quantity
+        product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
       redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
